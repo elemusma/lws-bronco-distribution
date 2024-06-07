fb932e9f40ea075671c27839fae90d455c9cc551
Efrain Lemus <efraintlemusm@gmail.com>
2024-06-07 00:26:23 -0600

    work on header navigation

--- assets/js/custom/nav.js
+++ assets/js/custom/nav.js
@@ -1,18 +1,31 @@
-// alert('hello from js/custom/nav.js');
-// console.log('hello from js/custom/nav.js');
 const businessToBusiness = document.querySelector('.business-to-business');
 const headerCustom = document.querySelector('.header');
 const bodyCustom = document.querySelector('body');
 
 document.querySelector('.business-to-business .close').addEventListener('click', function() {
-    // console.log(businessToBusiness);
-    // console.log(headerCustom);
-    // console.log(bodyCustom);
     headerCustom.style.height = `${headerCustom.offsetHeight - 20}px`;
-    // headerCustom.style.paddingTop = `20px`;
     bodyCustom.style.paddingTop = `${headerCustom.offsetHeight - 20}px`;
     businessToBusiness.style.opacity = 0;
     businessToBusiness.style.marginBottom = 0;
     businessToBusiness.style.pointerEvents = "none";
-    // businessToBusiness.style.display = "none";
-})
+});
+
+let businessToBusinessHeight = `${businessToBusiness.offsetHeight + 20}`;
+businessToBusiness.style.top = `-${businessToBusinessHeight}px`;
+headerCustom.style.top = `${businessToBusinessHeight}px`;
+bodyCustom.style.marginTop = `${businessToBusinessHeight}px`;
+
+window.addEventListener('scroll',customNav);
+
+function customNav() {
+    if(scrollY < `${businessToBusinessHeight}`) {
+        console.log('scrollY less than');
+        businessToBusiness.style.top = `-${businessToBusinessHeight}px`;
+        headerCustom.style.top = `${businessToBusinessHeight}px`;
+        bodyCustom.style.marginTop = `${businessToBusinessHeight}px`;
+    } else if (scrollY > `${businessToBusinessHeight}`) {
+        console.log('scrollY greater than');
+        headerCustom.style.top = `0px`;
+        bodyCustom.style.marginTop = `0px`;
+    }
+}
