9f3251a2fd319561c75371f950c3700ba0e56876
Efrain Lemus <efraintlemusm@gmail.com>
2024-06-11 15:53:07 -0600

    update product card and ask an expert

--- assets/owl-carousel/owl-carousels.js
+++ assets/owl-carousel/owl-carousels.js
@@ -32,34 +32,15 @@ $('.home-carousel').owlCarousel({
 });
 
 $('.featured-products-carousel').owlCarousel({
-    // center: true,
     loop: false,
     margin: 25,
     nav: true,
     dots: false,
-    // autoHeight: false,
-    // autoHeightClass: 'owl-height',
-    // stagePadding:170,
     autoplay: false,
     autoplayTimeout: 2500,
     autoplaySpeed: 5000, // this seems to make it autoscroll
     autoplayHoverPause: false,
     animateIn: 'fadeIn',
     animateOut: 'fadeOut',
-    // navText : ["<img src='/wp-content/uploads/2021/07/Arrow-Left-Blair-ITC.png' />","<img src='/wp-content/uploads/2021/07/Arrow-Right-Blair-ITC.png' />"],
     items:1,
-    // responsive: {
-    //     0: {
-    //         items: 2,
-    //         // slideBy: 2
-    //     },
-    //     600: {
-    //         items: 3,
-    //         // slideBy: 3
-    //     },
-    //     1000: {
-    //         items: 5,
-    //         // slideBy: 4
-    //     }
-    // }
 });
